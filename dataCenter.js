6193288851a0d4950c1d17214ef688b47c0f0f2c
Frenkieli <49364993+Frenkieli@users.noreply.github.com>
2021-10-04 17:40:34 +0800

    feat: 建立資料中心

---
+++ dataCenter.js
@@ -0,0 +1,61 @@
+const GetInstanceClass = require("./component/GetInstanceClass");
+/**
+ * @description 資料都存在這邊
+ *
+ * @class DataCenter
+ */
+class DataCenter extends GetInstanceClass {
+  constructor() {
+    super();
+
+    this.state = {};
+  }
+
+  /**
+   * @description 獲取對應資料
+   *
+   * @param {string} key
+   * @return {any} state data
+   * @memberof DataCenter
+   */
+  getData(key) {
+    switch (key) {
+      case "example":
+        return this.state[key];
+      default:
+        break;
+    }
+  }
+
+  /**
+   * @description 設定 state 資料
+   *
+   * @param {string} key
+   * @param {any} data state data
+   * @memberof DataCenter
+   */
+  setData(key, data) {
+    switch (key) {
+      case "example":
+        this.state[key] = data;
+        break;
+      default:
+        break;
+    }
+  }
+
+  /**
+   * @description 訂閱資料變更後要執行的程式,先預留到時候再寫
+   *
+   * @param {string} key state data
+   * @param {function} callBack
+   * @memberof DataCenter
+   */
+  subscription(key, callBack) {
+
+  }
+}
+
+let dataCenter = DataCenter.getInstance();
+
+module.exports = dataCenter;
